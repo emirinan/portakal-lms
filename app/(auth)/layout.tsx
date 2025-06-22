eb9b925b1bc5f4b3a94705b494109e08885ed519
Emir Inan <emirinandev@gmail.com>
2025-06-22 09:38:42 +0300

    feat: authentication & database

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,47 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/orange-logo.png";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link
+        href="/"
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        Back
+      </Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          className="flex items-center gap-2 self-center font-medium"
+          href="/"
+        >
+          <Image src={Logo} width={32} height={32} alt="Logo" />
+          Portakal LMS.
+        </Link>
+        {children}
+
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <span className="hover:text-primary hover:underline cursor-pointer">
+            Terms of Service
+          </span>{" "}
+          and{" "}
+          <span className="hover:text-primary hover:underline cursor-pointer">
+            Privacy Policy
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
