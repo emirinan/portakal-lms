8bc8fc8c0cf3d9386b0aa970c9e6d51c2805d341
Emir Inan <emirinandev@gmail.com>
2025-06-26 07:04:01 +0300

    feat: add course creation page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,3 +1,18 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function CoursesPage() {
-  return <h1>this is courses page</h1>;
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+        <Link href="/admin/courses/create" className={buttonVariants()}>
+          Create Course
+        </Link>
+      </div>
+      <div>
+        <h1>Here you will see all of the courses</h1>
+      </div>
+    </>
+  );
 }
---
+++ lib/zodSchemas.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+
+export const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
+
+export const courseStatus = ["Draft", "Published", "Archived"] as const;
+
+export const courseCategories = [
+  "Development",
+  "Business",
+  "Finance",
+  "IT & Software",
+  "Office Productivity",
+  "Personal Development",
+  "Design",
+  "Marketing",
+  "Health & Fitness",
+  "Music",
+  "Academics",
+] as const;
+
+export const courseSchema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must be at least 3 characters long" })
+    .max(100, { message: "Title must be at most 100 characters long" }),
+  description: z
+    .string()
+    .min(3, { message: "Description must be at least 3 characters long" }),
+  fileKey: z.string().min(1, { message: "File is required" }),
+  price: z.coerce
+    .number()
+    .min(1, { message: "Price must be a positive number" }),
+  duration: z.coerce
+    .number()
+    .min(1, { message: "Duration must be at least 1 hour" })
+    .max(500, { message: "Duration must be at most 500 hour" }),
+  level: z.enum(courseLevels, { message: "Level is required" }),
+  category: z.enum(courseCategories, { message: "Category is required" }),
+  smallDescription: z
+    .string()
+    .min(3, { message: "Small Description must be at least 3 characters long" })
+    .max(200, {
+      message: "Small Description must be at most 200 characters long",
+    }),
+  slug: z
+    .string()
+    .min(3, { message: "Slug must be at least 3 characters long" }),
+  status: z.enum(courseStatus, { message: "Status is required" }),
+});
+
+export type CourseSchemeType = z.infer<typeof courseSchema>;
